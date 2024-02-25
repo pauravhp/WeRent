8e0305c7536802b26520637058bec85e4d16eb56
Paurav H Param <pauravhp2003@gmail.com>
2024-02-25 00:33:47 -0800

    Read and display data from MongoDb in a simple manner

--- we-rent-app/server/server.js
+++ we-rent-app/server/server.js
@@ -1,39 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.use(cors());
 
 //MONGODB CONNECTION
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.dewuoen.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+	"mongodb+srv://Paurav:werent@cluster0.dewuoen.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 });
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+
+		app.get("/api/listings", async (req, res) => {
+			try {
+				await client.connect();
+				console.log("Connected to MongoDB");
+
+				const db = client.db("WeRent");
+				const collection = db.collection("Listings");
+				const listings = (await collection.find({}).toArray()).slice(1);
+				res.json(listings);
+				console.log("The listings: ", listings);
+			} catch (error) {
+				console.error("Error fetching listings:", error);
+				res.status(500).json({ error: "Internal Server Error" });
+			}
+		});
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
 }
 run().catch(console.dir);
 
-
 //referring to rating js file
-const rating = require('./routes/rating');
+// const rating = require("./routes/rating");
 
-//defining the base of other api endpoints
-app.use('/api/', rating);
+// //defining the base of other api endpoints
+// app.use("/api/", rating);
 
-// http://localhost:2345
-app.listen(2345);
-console.log("The server is running now!");
-console.log("click here to see the sample data in c1 ----> http://localhost:2345/api/c1");
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
--- we-rent-app/src/App.tsx
+++ we-rent-app/src/App.tsx
@@ -1,76 +1,65 @@
-import { Typography, Box, Container, Paper, styled, AppBar } from '@mui/material';
+import {
+	Typography,
+	Box,
+	Container,
+	Paper,
+	styled,
+	AppBar,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 
-/*
-RUN THIS COMMAND:
-npm install @mui/material/Typography @mui/material/Box @mui/material/Container @mui/material/Paper @mui/material/styled @mui/material/AppBar
-*/
-function App() {
-
-  const MyBar = styled(AppBar)(({ theme }) => ({
-    backgroundColor: 'black', // Change background color to black
-  }));
-
-  return (
-    <Container>
-      <Container>
-        <MyBar sx={{ p: 1, textAlign: "left" }} >
-          <Typography variant="h4" sx={{ p: 1, textAlign: "left" }}> WERENT</Typography>
-        </MyBar>
-        <Typography variant="h4" sx={{ p: 1, textAlign: "center" }}> testing</Typography>
-      </Container>
-
-      <Box sx={{ pt: 4, display: 'flex', flexDirection: 'row', justifyContent: "space-between", gap: 4 }}>
-
-        <Paper elevation={24}>
-          <Typography variant="h3" sx={{ p: 1, textAlign: "center" }}> FOR RENT</Typography>
-
-          <Typography variant="h6" sx={{ p: 1, textAlign: "left" }}>
-            Address: 123 Main St, Anytown, USA
-            <br />
-            Price: $1,500 per month
-
-            <br />
-            Location: Anytown, USA
-            <br />
-            <br />
-            Description: Welcome to 123 Main St, a charming two-bedroom, one-bathroom apartment located in the heart of Anytown, USA. This unit boasts a spacious living area, a fully-equipped kitchen, and large windows that let in plenty of natural light. The bedrooms are generously-sized and feature ample closet space.
-            <br />
-            <br />
-            Amenities:&emsp;&bull; Fully-furnished <br /> &emsp;&bull; Washer/Dryer in unit <br /> &emsp;&bull; Central A/C and heating <br /> &emsp;&bull; Private balcony<br /> &emsp;&bull;Secure building with intercom system
-            <br />
-            <br />
-            Nearby: Walking distance to Anytown University <br /> &emsp;&bull; Close to restaurants, shops, and parks <br /> &emsp;&bull;Easy access to public transportation
-            <br />
-            <br />
-            This apartment is perfect for students or young professionals looking for a convenient and comfortable living space in a vibrant neighborhood. Contact us today to schedule a viewing!</Typography>
-        </Paper>
-        <Paper elevation={24}>
-          <Typography variant="h3" sx={{ p: 1, textAlign: "center" }}> FOR RENT</Typography>
-          <Typography variant="h6" sx={{ p: 1, textAlign: "left" }}>
-            Address: 456 Elm St, Anytown, USA
-            <br />
-            Price: $1,800 per month
-            <br />
-            Location: Anytown, USA
-            <br />
-            <br />
-            Description: Welcome to 456 Elm St, a charming two-bedroom, one-bathroom apartment located in the heart of Anytown, USA. This unit boasts a spacious living area, a fully-equipped kitchen, and large windows that let in plenty of natural light. The bedrooms are generously-sized and feature ample closet space.
-            <br />
-            <br />
-            Amenities:&emsp;&bull; Fully-furnished <br /> &emsp;&bull; Washer/Dryer in unit <br /> &emsp;&bull; Central A/C and heating <br /> &emsp;&bull; Private balcony<br /> &emsp;&bull;Secure building with intercom system
-            <br />
-            <br />
-            Nearby: Walking distance to Anytown University <br /> &emsp;&bull; Close to restaurants, shops, and parks <br /> &emsp;&bull;Easy access to public transportation
-            <br />
-            <br />
-            This apartment is perfect for students or young professionals looking for a convenient and comfortable living space in a vibrant neighborhood. Contact us today to schedule a viewing!</Typography>
-        </Paper>
-      </Box>
-    </Container>
-
-  )
-
+interface Listing {
+	address: string;
+	price: number;
+	numBedrooms: number;
 }
 
-export default App;
+const fetchDataFromDb = async (
+	endpoint: string,
+	setData: (listings: Listing[]) => void
+) => {
+	try {
+		const response = await fetch(`http://localhost:3001${endpoint}`);
+		const result = await response.json();
+		setData(result);
+	} catch (error) {
+		console.error(`Error fetching data from ${endpoint}:`, error);
+	}
+};
 
+const App = () => {
+	const MyBar = styled(AppBar)(({ theme }) => ({
+		backgroundColor: "black", // Change background color to black
+	}));
+
+	const [listings, setListings] = useState<Listing[] | null>(null);
+
+	useEffect(() => {
+		fetchDataFromDb("/api/listings", setListings);
+	}, []);
+
+	return (
+		<Container>
+			<MyBar sx={{ p: 1, textAlign: "left" }}>
+				<Typography variant="h4" sx={{ p: 1, textAlign: "left" }}>
+					{" "}
+					WERENT
+				</Typography>
+			</MyBar>
+			{listings &&
+				listings.map((listing, i) => (
+					<Typography
+						variant="h4"
+						sx={{ p: 1, textAlign: "center", margin: 10 }}
+						key={i}
+					>
+						{listing.address}
+						{listing.price}
+						{listing.numBedrooms}
+					</Typography>
+				))}
+		</Container>
+	);
+};
+
+export default App;
